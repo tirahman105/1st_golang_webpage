16e7a14f10ee7d3d2c9463030b8b303df36b2a50
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2021-03-20 20:36:18 +0600

    1st commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/contact", contact)
+	http.ListenAndServe(":8080", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, `Welcome to my first golang webpage`)
+}
+func about(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, `Welcome to my first about webpage`)
+}
+func contact(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, `Welcome to my first contact webpage`)
+}
